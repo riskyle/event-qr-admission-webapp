7d32b0e120fb6e59541cbac1b895dbe42ca8c6c4
Christian Kyle Autor <christian@sos.com>
2024-10-15 05:08:55 +0800

    deploy

--- middleware.ts
+++ middleware.ts
@@ -8,9 +8,6 @@ export function middleware(req: NextRequest) {
   const basicAuth = req.headers.get("authorization");
   const url = req.nextUrl;
 
-  console.log(process.env.USER_NAME);
-  console.log(process.env.PASSWORD);
-
   if (basicAuth) {
     const authValue = basicAuth.split(" ")[1];
     const [user, pwd] = atob(authValue).split(":");
