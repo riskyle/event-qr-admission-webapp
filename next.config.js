ea54ea717640119634d329826555e487b2685fd6
Christian Kyle Autor <christian@sos.com>
2024-10-14 23:45:57 +0800

    deploy

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'storage.googleapis.com',
+                port: '',
+                pathname: '/event-qr-admission.appspot.com/**',
+            },
+        ],
+    },
+}
