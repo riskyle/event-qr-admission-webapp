b32bcfe4d4f72f8765c7496b834ab04cfa5b6182
Christian Kyle Autor <christian@sos.com>
2024-10-14 22:36:49 +0800

    deploy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head';
+import "./styles/globals.css"
 
 export default function MyApp({ Component, pageProps }: AppProps) {
 
@@ -7,7 +8,6 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Glitz & Glam SOS</title>
-
       </Head>
       <Component {...pageProps} />
     </>
