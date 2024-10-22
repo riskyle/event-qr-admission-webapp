920cf95fe653e089cd327328dc555e14a2d548a8
Christian Kyle Autor <christian@sos.com>
2024-10-23 07:26:52 +0800

    init

--- firebase/index.ts
+++ firebase/index.ts
@@ -1,26 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {
-    getFirestore,
-    collection,
-    query,
-    where,
-    orderBy,
-    limit,
+  getFirestore,
+  collection,
+  query,
+  where,
+  orderBy,
+  limit,
 } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDroR37bkWLjKQCVixOKMgU0xOh9xxrg0I",
-    authDomain: "event-qr-admission.firebaseapp.com",
-    projectId: "event-qr-admission",
-    storageBucket: "event-qr-admission.appspot.com",
-    messagingSenderId: "482730707395",
-    appId: "1:482730707395:web:5a0a62c36cf32243609576",
-    measurementId: "G-ZNTBG2Y8XM",
+  apiKey: "AIzaSyBKx9ox4_r82rZjc-yQhdkb4qdnQ3uERYQ",
+  authDomain: "event-qr-admission-backer.firebaseapp.com",
+  projectId: "event-qr-admission-backer",
+  storageBucket: "event-qr-admission-backer.appspot.com",
+  messagingSenderId: "834742315666",
+  appId: "1:834742315666:web:c2da602c2d6f133da5544b",
 };
 
 // Initialize Firebase
@@ -30,8 +28,8 @@ const db = getFirestore();
 
 const employeeDocRef = collection(db, "employee");
 export const queryEmployee = query(
-    employeeDocRef,
-    where("isScanned", "==", true),
-    orderBy("scannedAt", "desc"),
-    limit(1)
+  employeeDocRef,
+  where("isScanned", "==", true),
+  orderBy("scannedAt", "desc"),
+  limit(1)
 );
