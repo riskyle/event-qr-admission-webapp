bab2e2e8bc6015e9e17b6875e8d9e563c9e5fbdb
Christian Kyle Autor <christian@sos.com>
2024-11-08 22:58:09 +0800

    original db

--- firebase/index.ts
+++ firebase/index.ts
@@ -13,25 +13,25 @@ import {
 
 // Your web app's Firebase configuration
 // !!!!!ORIGINAL FIREBASE!!!!!!
-// const firebaseConfig = {
-//   apiKey: process.env.FIREBASE_API,
-//   authDomain: "event-qr-admission.firebaseapp.com",
-//   projectId: "event-qr-admission",
-//   storageBucket: "event-qr-admission.appspot.com",
-//   messagingSenderId: "482730707395",
-//   appId: "1:482730707395:web:5a0a62c36cf32243609576",
-//   measurementId: "G-ZNTBG2Y8XM",
-// };
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API,
+  authDomain: "event-qr-admission.firebaseapp.com",
+  projectId: "event-qr-admission",
+  storageBucket: "event-qr-admission.appspot.com",
+  messagingSenderId: "482730707395",
+  appId: "1:482730707395:web:5a0a62c36cf32243609576",
+  measurementId: "G-ZNTBG2Y8XM",
+};
 
 //  !!!!BACKER FIREBASE!!!!
-const firebaseConfig = {
-  apiKey: process.env.BACKER_FIREBASE_API,
-  authDomain: "event-qr-admission-backer.firebaseapp.com",
-  projectId: "event-qr-admission-backer",
-  storageBucket: "event-qr-admission-backer.appspot.com",
-  messagingSenderId: "834742315666",
-  appId: "1:834742315666:web:c2da602c2d6f133da5544b",
-};
+// const firebaseConfig = {
+//   apiKey: process.env.BACKER_FIREBASE_API,
+//   authDomain: "event-qr-admission-backer.firebaseapp.com",
+//   projectId: "event-qr-admission-backer",
+//   storageBucket: "event-qr-admission-backer.appspot.com",
+//   messagingSenderId: "834742315666",
+//   appId: "1:834742315666:web:c2da602c2d6f133da5544b",
+// };
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
